5c0b2ee2b61633eac10af85e3d40c322ea5ab633
yatamci <yatamci@yahoo.com>
2026-02-16 16:34:10 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(__dirname));
+
+// Serve index.html for all routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server läuft auf Port ${PORT}`);
+    console.log(`Öffne http://localhost:${PORT} in deinem Browser`);
+});
